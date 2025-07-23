4ac9af283d6039e91752a47e3a7ef16a21d8c12b
omartr22 <trabomar76@gmail.com>
2025-07-23 13:27:33 -0400

    Implemented list all accounts route

--- service/routes.py
+++ service/routes.py
@@ -60,7 +60,16 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
+    """
+    List all Accounts
+    This endpoint will return all Accounts
+    """
+    app.logger.info("Request to list Accounts")
+    accounts = Account.all()
+    results = [account.serialize() for account in accounts]
+    return jsonify(results), status.HTTP_200_OK
 # ... place you code here to LIST accounts ...
 
 
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -136,4 +136,12 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+    def test_list_all_accounts(self):
+        """It should List all Accounts"""
+        self._create_accounts(3)  # helper function to make 3 accounts
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 3)
+
 
